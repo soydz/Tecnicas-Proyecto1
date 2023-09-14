78d2653f96fbd061efd78f5d16c3313dc5ab0ba7
soydz <soydz@pm.me>
2023-09-13 22:32:04 -0500

    Se cambian nombres de variables para mejorar la legibilidad

--- Proyecto1.java
+++ Proyecto1.java
@@ -3,23 +3,24 @@ import java.util.ArrayList;
 
 class Proyecto1 {
 
-  record pais(String pais,int poblacion,double porcentajeAccesoAguaPotable,int poblacionAfectadaSequias, int numeroHospitales,int hospitalesProblemasDeSaneamiento, double numeroPlantasPotabilizadorasPor100mh){};
+  record Pais(String pais,int poblacion,double porcentajeAccesoAguaPotable,int poblacionAfectadaSequias, int numeroHospitales,int hospitalesProblemasDeSaneamiento, double numeroPlantasPotabilizadorasPor100mh){};
 
   public static void main(String[] args){
-    List<pais> datosPais = new ArrayList<pais>();
-    datosPais.add( new pais("Colombia", 50340000, 72.3, 4500000, 3500,900, 15.7) );
-    datosPais.add( new pais("Argentina", 46044703, 88.6, 1400000, 4200, 150, 32.1) );
-    datosPais.add( new pais("Bolivia", 11216000, 61.9, 1600000, 1021,400, 9.7) );
-    datosPais.add( new pais("Chile", 17574003, 92.6, 958100, 3200, 98, 48.9) );
-    datosPais.add( new pais("Venezuela", 32605423, 24.5, 10800000, 1207, 2100, 14.8) );
-    datosPais.add( new pais("Haití", 11488792, 16.3, 950000, 420, 950, 5.2) );
-    datosPais.add( new pais("Uruguay", 3286314, 97.9, 410, 3130, 45, 21.4) );
+    List<Pais> listaPaises = new ArrayList<Pais>();
+    listaPaises.add( new Pais("Colombia", 50340000, 72.3, 4500000, 3500,900, 15.7) );
+    listaPaises.add( new Pais("Argentina", 46044703, 88.6, 1400000, 4200, 150, 32.1) );
+    listaPaises.add( new Pais("Bolivia", 11216000, 61.9, 1600000, 1021,400, 9.7) );
+    listaPaises.add( new Pais("Chile", 17574003, 92.6, 958100, 3200, 98, 48.9) );
+    listaPaises.add( new Pais("Venezuela", 32605423, 24.5, 10800000, 1207, 2100, 14.8) );
+    listaPaises.add( new Pais("Haití", 11488792, 16.3, 950000, 420, 950, 5.2) );
+    listaPaises.add( new Pais("Uruguay", 3286314, 97.9, 410, 3130, 45, 21.4) );
 
 
   
-    System.out.println(datosPais.get(0).pais);
+    //System.out.println(datosPais.get(0).pais);
 
   }
+  /*
   public static double promedioAccesoAgua(List<Pais> listaPaises){
         int totalPaises = listaPaises.size();
         double suma = 0;
@@ -28,4 +29,5 @@ class Proyecto1 {
         }
         return suma / totalPaises;
         }
+        */
 }
